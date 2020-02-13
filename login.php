c95c6d0d098b0af260f77de04b037f9f3a1fa82f
Igbinyemi Adeboye Amos <41273749+ENADSOFT@users.noreply.github.com>
2020-02-13 12:13:07 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
+  <!-- Mobile Specific Meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="images/mosesmed favicon.png">
+  <!-- Author Meta -->
+  <meta name="Moses Med" content="Freelance full stack website developer and designer">
+  <!-- Meta Description -->
+  <meta name="description" content="">
+  <!-- Meta Keyword -->
+  <meta name="keywords" content="">
+  <!-- meta character set -->
+  <meta charset="UTF-8">
+  <!-- Site Title -->
+  <title>Moses Med | Official Page | Home</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+    <!--
+    CSS
+    ============================================= -->
+    <link rel="stylesheet" href="ass/css/linearicons.css">
+    <link rel="stylesheet" href="ass/css/font-awesome.min.css">
+    <link rel="stylesheet" href="ass/css/bootstrap.css">
+    <link rel="stylesheet" href="ass/css/magnific-popup.css">
+    <link rel="stylesheet" href="ass/css/jquery-ui.css">
+    <link rel="stylesheet" href="ass/css/nice-select.css">
+    <link rel="stylesheet" href="ass/css/animate.min.css">
+    <link rel="stylesheet" href="ass/css/owl.carousel.css">
+    <link rel="stylesheet" href="ass/css/main.css">
+  </head>
+<body>
+
+  <header id="header">
+    <div class="container main-menu">
+      <div class="row align-items-center justify-content-between d-flex">
+        <div id="logo">
+          <a href="index.html"><img src="images/mosesmed logo.png" alt="logo" title="" /></a>
+        </div>
+        <nav id="nav-menu-container">
+          <ul class="nav-menu">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="login.html">Login</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header><br /><br /><br /><br />
+
+  <!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
+  <!-- Mobile Specific Meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="FROG.jpg">
+  <!-- Author Meta -->
+  <meta name="OLOMOLA TEJUMADE" content="GRAPHIC DESIGNER AND EVENT PLANNER">
+  <!-- Meta Description -->
+  <meta name="description" content="">
+  <!-- Meta Keyword -->
+  <meta name="keywords" content="">
+  <!-- meta character set -->
+  <meta charset="UTF-8">
+  <!-- Site Title -->
+  <title>ENAD | Official Page | Home</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+    <!--
+    CSS
+    ============================================= -->
+    <link rel="stylesheet" href="ass/css/linearicons.css">
+    <link rel="stylesheet" href="ass/css/font-awesome.min.css">
+    <link rel="stylesheet" href="ass/css/bootstrap.css">
+    <link rel="stylesheet" href="ass/css/magnific-popup.css">
+    <link rel="stylesheet" href="ass/css/jquery-ui.css">
+    <link rel="stylesheet" href="ass/css/nice-select.css">
+    <link rel="stylesheet" href="ass/css/animate.min.css">
+    <link rel="stylesheet" href="ass/css/owl.carousel.css">
+    <link rel="stylesheet" href="ass/css/main.css">
+  </head>
+  <body>
+
+    <header id="header">
+      <div class="container main-menu">
+        <div class="row align-items-center justify-content-between d-flex">
+          <div id="logo">
+            <a href="index.html"><img src="FROG.jpg" alt="logo" title="" /></a>
+          </div>
+          <nav id="nav-menu-container">
+            <ul class="nav-menu">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="contact.html">Contact</a></li>
+              <li><a href="login.html">Login</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+
+
+    <div class="container">
+      <hr>
+    </div>
+    <main>
+
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-sm-8 col-md-6 col-lg-4 offset-sm-2 offset-md-3 offset-lg-4">
+              <h1 class="mb-3 text-center">Admin Login Page</h1>
+              <form class="mb-3" action="login.php" method="post">
+                <div class="form-group">
+                  <label for="username">Username:</label>
+                  <input type="text" name="username" class="form-control" placeholder="name" id="username" required>
+                </div>
+                <div class="form-group">
+                  <label for="password">Password:</label>
+                  <input type="text" name="password" class="form-control" id="password" required>
+                </div>
+                <input type="submit" class="btn btn-primary btn-block" name="login" value="Login">
+              </form>
+              <div class="text-center">
+                <p class="small">This form is for admin use only</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </main>
+
+    <footer id="mu-footer" role="contentinfo">
+			<div class="container">
+				<div class="mu-footer-area">
+					<p class="mu-copy-right">&copy; 2020 ENADCSITS | Made by <a rel="nofollow" href="http://markups.io">EAND</a>.</p>
+				</div>
+			</div>
+
+	</footer>
+
+    <script src="js/vendor/jquery-2.2.4.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+      <script src="js/easing.min.js"></script>
+    <script src="js/hoverIntent.js"></script>
+    <script src="js/superfish.min.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+      <script src="js/jquery.tabs.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+          <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/simple-skillbar.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/mail-script.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+  </html>
+
+
+
+<?php
+
+
+if (isset($_POST['login'])) {
+  $user=$_POST['username'];
+  $pass=$_POST['password'];
+
+  if (empty($user) || empty($pass)) {
+    echo 'Please fill all the field';
+  }else {
+    $dbc=mysqli_connect('localhost','root','','enad')or die('unable to connect to database');
+    $q="SELECT * FROM admin WHERE username='$user' AND password='$pass'";
+    $run=mysqli_query($dbc,$q)or die('unable to connect to the database');
+    $num_rows=mysqli_num_rows($run);
+    if ($num_rows==1) {
+      $row=mysqli_fetch_array($run);
+      $username=$row['username'];
+      $password=$row['password'];
+      $email=$row['email'];
+      $phone=$row['phone'];
+      $address=$row['address'];
+      if ($user==$username && $pass==$password) {
+        echo 'Welcome to the admin dashboard<br />';
+        echo 'Username: ' . $username . '<br />';
+        echo 'Password: ' . $password . '<br />';
+        echo 'Email: ' . $email . '<br />';
+        echo 'Phone: ' . $phone . '<br />';
+        echo 'Address: ' . $address . '<br />';
+        echo '<a href="edit.html">change contact details</a><br />';
+      }else {
+        echo 'Unrecognised login details';
+      }
+    }else {
+      echo'unable to login';
+    }
+  }
+}
+
+
+
+
+ ?>
+<footer class="footer-area section-gap">
+     < div class="container">
+     <p class="mu-copy-right">&copy; 2020 ENADCSITS | Made by <a rel="nofollow" href="http://markups.io">EAND</a>.</p>
+       <a href="#"></a>
+     </div>
+ </footer>
+</body>
+</html>
